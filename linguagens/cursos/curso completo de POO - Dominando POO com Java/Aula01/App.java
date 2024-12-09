6ce251582a6bcd03d997ead8185363648b14e552
gsyssfdyst <gsyssfdyst@gmail.com>
2024-12-08 23:01:52 -0300

    Test Tablet

--- linguagens/cursos/curso completo de POO - Dominando POO com Java/Aula01/App.java
+++ linguagens/cursos/curso completo de POO - Dominando POO com Java/Aula01/App.java
@@ -49,7 +49,7 @@ public class App {
        System.out.println(novoCarro.getModelo());
        System.out.println(novoCarro.getAno());
 
-       System.out.println("Teste")
+       System.out.println("Teste");
        System.out.println(novoCarro.getCor());
 
 
@@ -58,7 +58,7 @@ public class App {
 }
 
 /*
- * Formas de Escrever: 
+ * Formas de Escrever: ;
  * Pasacalcase: MeuCachorro/NotaFiscal palavra composta, palavra unica: Cschorro
  * CamelCase: Todo palavra comeca com letra minuscula, como se fosse um camelo 
  * Kibab-case: mnota-fiscal
