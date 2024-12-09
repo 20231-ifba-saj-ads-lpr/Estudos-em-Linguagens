da4337cc39cca72e42a27db42b10b291e7195a32
20222TADSSAJ0013 <20222tadssaj0013@ifba.edu.br>
2024-12-08 23:15:53 -0300

    Organizacão dos arquivos

--- linguagens/cursos/java.poo/doZeroaoProfissionalProjetos/src/fundamentos/PrimeiroPrograma.java
+++
@@ -1,7 +0,0 @@
-package cursos.java.poo.doZeroaoProfissionalProjetos.src.fundamentos;
-
-public class PrimeiroPrograma {
-    public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
-    }
-}
